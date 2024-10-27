1546193d5c5e47d0669e3e688160c37a49738dd1
YASSIR BELFATHI <belfathiy@gmail.com>
2024-10-27 21:40:53 +0100

    initalisation du projet inventaire vol

---
+++ vol/vol/src/main/java/com/inventaire/vol/VolApplication.java
@@ -0,0 +1,17 @@
+package com.inventaire.vol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import  java.util.Scanner;
+
+@SpringBootApplication
+public class VolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolApplication.class, args);
+
+		Scanner scan=new Scanner(System.in);
+
+	}
+
+}
