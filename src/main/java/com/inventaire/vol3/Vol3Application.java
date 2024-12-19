87605aa191fc1a5990a756c435d01d272339dab3
yassir belfathi <belfathiy@gmail.com>
2024-12-19 22:00:39 +0100

    initialisation du projet inventaire_project test3

---
+++ src/main/java/com/inventaire/vol3/Vol3Application.java
@@ -0,0 +1,13 @@
+package com.inventaire.vol3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vol3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vol3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/inventaire/vol3/Vol3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventaire.vol3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vol3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
